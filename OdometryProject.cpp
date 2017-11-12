826885bf16da2a62da708eecb5cf2e9fc15d86f7
leifsahyun <33590091+leifsahyun@users.noreply.github.com>
2017-11-12 01:07:16 -0500

    Add files via upload

---
+++ OdometryProject.cpp
@@ -0,0 +1,21 @@
+#include "OdometryProject.h"
+
+void OdometryProject::update(){
+  int leftDiff = leftEncoder->read() - lastLeftReading;
+  int rightDiff = rightEncoder->read() - lastRightReading;
+  double avgDiff = (leftDiff + rightDiff)/2.0;
+  x = cos(heading)*avgDiff;
+  y = sin(heading)*avgDiff;
+  gyro->read();
+  heading = gyro->g.z;
+}
+
+void OdometryProject::zero(){
+  x=0;
+  y=0;
+  heading = 0;
+  leftEncoder->write(0);
+  rightEncoder->write(0);
+  //gyro->zero Need to find a way to zero the gyro here
+}
+
---
+++ OdometryProject.h
@@ -0,0 +1,18 @@
+#include <L3G.h>
+#include <Encoder.h>
+#include "Arduino.h"
+
+namespace OdometryProject {
+  public:
+    double x;
+    double y;
+    double heading;
+    long lastLeftReading = 0;
+    long lastRightReading = 0;
+    Encoder *leftEncoder;
+    Encoder *rightEncoder;
+    L3G *gyro;
+    void update();
+    void zero();    
+}
+
